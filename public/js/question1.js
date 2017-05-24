e21d3dd5b72f273f2b82d312c580b3e6de5a428d
Xindi LI <xil573@eng.ucsd.edu>
2017-05-24 13:06:01 -0700

    add timer and save time in q1_response

--- public/js/question1.js
+++ public/js/question1.js
@@ -5,3 +5,20 @@ function gotoQuestionnaire(){
 function gotoQuestion2(){
 	window.location.href = "question2";
 }
+
+function two_char(n) {
+    return n >= 10 ? n : "0" + n;
+}
+
+var c = 0;
+var t;
+function timeCount(){
+	document.getElementById('mytime').value = c;
+	c = c + 1;
+	t = setTimeout("timeCount()", 1000);
+}
+
+function stopCount(){
+	var duration = document.getElementById("mytime").value;
+	console.log("Question 1 duration:", duration);
+}
